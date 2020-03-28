8f1f8f5f1f2b6d85d789e63a595bf4ca018a10a2
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-28 09:55:26 +0100

    Error fix in MUI import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createMuiTheme from '@material-ui/core/createMuiTheme';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 //Components
 import Navbar from './components/Navbar';
