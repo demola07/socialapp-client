c02561e620efd3f4665aeb28be137ad8df922c5f
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-28 13:05:44 +0100

    Fixed bug with spacing grid layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -29,7 +29,7 @@ class home extends Component {
       <p>Loading...</p>
     );
     return (
-      <Grid container spacing={16}>
+      <Grid container spacing={3}>
         <Grid item sm={8} xs={12}>
           {recentScreamsMarkup}
         </Grid>
