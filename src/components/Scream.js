356351e4ac45473530b8525096ae5945bcf61173
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-31 00:32:17 +0100

    implementing user like and unlike in scream component

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import MyButton from '../util/MyButton';
 
 //MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -8,8 +11,16 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
-import dayjs from 'dayjs';
-var relativeTime = require('dayjs/plugin/relativeTime');
+//Icons
+import ChatIcon from '@material-ui/icons/Chat';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+
+//Redux
+import { connect } from 'react-redux';
+import { likeScream, unlikeScream } from '../redux/actions/dataActions';
+
+const relativeTime = require('dayjs/plugin/relativeTime');
 
 const styles = {
   card: {
@@ -26,36 +37,52 @@ const styles = {
 };
 
 class Scream extends Component {
+  likedScream = () => {
+    if (
+      this.props.user.likes &&
+      this.props.user.likes.find(like => like.screamId === this.props.scream.screamId)
+    )
+      return true;
+    else return false;
+  };
+
+  likeScream = () => {
+    this.props.likeScream(this.props.scream.screamId);
+  };
+  unlikeScream = () => {
+    this.props.unlikeScream(this.props.scream.screamId);
+  };
+
   render() {
     dayjs.extend(relativeTime);
 
     const {
       classes,
-      scream: {
-        body,
-        createdAt,
-        userImage,
-        userHandle,
-        screamId,
-        likeCount,
-        commentCount
-      }
+      scream: { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount },
+      user: { authenticated }
     } = this.props;
+
+    const likeButton = !authenticated ? (
+      <MyButton tip='Like'>
+        <Link to='/login'>
+          <FavoriteBorder color='primary' />
+        </Link>
+      </MyButton>
+    ) : this.likedScream() ? (
+      <MyButton tip='Undo like' onClick={this.unlikeScream}>
+        <FavoriteIcon color='primary' />
+      </MyButton>
+    ) : (
+      <MyButton tip='Like' onClick={this.likeScream}>
+        <FavoriteBorder color='primary' />
+      </MyButton>
+    );
 
     return (
       <Card className={classes.card}>
-        <CardMedia
-          image={userImage}
-          title='Profile image'
-          className={classes.image}
-        />
+        <CardMedia image={userImage} title='Profile image' className={classes.image} />
         <CardContent className={classes.content}>
-          <Typography
-            variant='h5'
-            component={Link}
-            to={`/users/${userHandle}`}
-            color='primary'
-          >
+          <Typography variant='h5' component={Link} to={`/users/${userHandle}`} color='primary'>
             {userHandle}
           </Typography>
 
@@ -64,10 +91,33 @@ class Scream extends Component {
           </Typography>
 
           <Typography variant='body1'>{body}</Typography>
+
+          {likeButton}
+          <span>{likeCount} Likes</span>
+          <MyButton tip='comments'>
+            <ChatIcon color='primary' />
+          </MyButton>
+          <span>{commentCount} Comments</span>
         </CardContent>
       </Card>
     );
   }
 }
+Scream.propTypes = {
+  user: PropTypes.object.isRequired,
+  likeScream: PropTypes.func.isRequired,
+  unlikeScream: PropTypes.func.isRequired,
+  scream: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
+};
 
-export default withStyles(styles)(Scream);
+const mapActionsToProps = {
+  likeScream,
+  unlikeScream
+};
+
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Scream));
