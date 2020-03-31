e8d797e22e7540f09ecf833fcdf49edf5e12ce17
demola07 <45898468+demola07@users.noreply.github.com>
2020-03-31 20:26:56 +0100

    styled dialog box for for displaying users scream

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -26,6 +26,19 @@ const styles = theme => ({
   invisibleSeparator: {
     border: 'none',
     margin: 4
+  },
+  profileImage: {
+    maxWidth: 200,
+    height: 200,
+    borderRadius: '50%',
+    objectFit: 'cover'
+  },
+  dialogContent: {
+    padding: 20
+  },
+  closeButton: {
+    position: 'absolute',
+    left: '90%'
   }
 });
 
