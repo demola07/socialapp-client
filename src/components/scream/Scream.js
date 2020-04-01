62d8a4beb2d29073cdcdd313f5467f4bb77515a9
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-01 23:57:39 +0100

    created route to view users scream via screamId

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -71,7 +71,11 @@ class Scream extends Component {
             <ChatIcon color='primary' />
           </MyButton>
           <span>{commentCount} Comments</span>
-          <ScreamDialog screamId={screamId} userHandle={userHandle} />
+          <ScreamDialog
+            screamId={screamId}
+            userHandle={userHandle}
+            openDialog={this.props.openDialog}
+          />
         </CardContent>
       </Card>
     );
@@ -81,7 +85,8 @@ class Scream extends Component {
 Scream.propTypes = {
   user: PropTypes.object.isRequired,
   scream: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  openDialog: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
--- src/components/scream/ScreamDialog.js
+++ src/components/scream/ScreamDialog.js
@@ -56,6 +56,12 @@ class ScreamDialog extends Component {
     open: false
   };
 
+  componentDidMount() {
+    if (this.props.openDialog) {
+      this.handleOpen();
+    }
+  }
+
   handleOpen = () => {
     this.setState({ open: true });
     this.props.getScream(this.props.screamId);
