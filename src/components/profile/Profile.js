9bf260b94a92cb04ae94f36ac09970ac5cefc279
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-02 02:44:41 +0100

    created skeleton for profile section while loading

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import EditDetails from './EditDetails';
 import MyButton from '../../util/MyButton';
+import ProfileSkeleton from '../../util/ProfileSkeleton';
 
 //MUI
 import MuiLink from '@material-ui/core/Link';
@@ -24,51 +25,7 @@ import { connect } from 'react-redux';
 import { uploadImage, logoutUser } from '../../redux/actions/userActions';
 
 const styles = theme => ({
-  paper: {
-    padding: 20
-  },
-  profile: {
-    '& .image-wrapper': {
-      textAlign: 'center',
-      position: 'relative',
-      '& button': {
-        position: 'absolute',
-        top: '80%',
-        left: '70%'
-      }
-    },
-    '& .profile-image': {
-      width: 200,
-      height: 200,
-      objectFit: 'cover',
-      maxWidth: '100%',
-      borderRadius: '50%'
-    },
-    '& .profile-details': {
-      textAlign: 'center',
-      '& span, svg': {
-        verticalAlign: 'middle'
-      },
-      '& a': {
-        color: theme.palette.primary.main
-      }
-    },
-    '& hr': {
-      border: 'none',
-      margin: '0 0 10px 0'
-    },
-    '& svg.button': {
-      '&:hover': {
-        cursor: 'pointer'
-      }
-    }
-  },
-  buttons: {
-    textAlign: 'center',
-    '& a': {
-      margin: '20px 10px'
-    }
-  }
+  ...theme.otherstyles
 });
 
 class Profile extends Component {
@@ -173,7 +130,7 @@ class Profile extends Component {
         </Paper>
       )
     ) : (
-      <p> Loading...</p>
+      <ProfileSkeleton />
     );
 
     return profileMarkup;
